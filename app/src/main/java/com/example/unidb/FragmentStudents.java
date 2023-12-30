a222d2521092260b67915755abc02ed6c94c0253
Orcun Selbasan <lutfuorcunselbasan@gmail.com>
2023-12-30 21:36:04 +0300

    Added student list

--- app/src/main/java/com/example/unidb/FragmentStudents.java
+++ app/src/main/java/com/example/unidb/FragmentStudents.java
@@ -1,16 +1,31 @@
 package com.example.unidb;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FragmentStudents extends Fragment {
     private Database db;
+    ListView list;
+    StudentListAdapter listAdapter;
+    ArrayList<HashMap> data = new ArrayList<>();
+    Button refresh;
 
     public FragmentStudents(Database db) {
         this.db = db;
@@ -19,7 +34,72 @@ public class FragmentStudents extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.students, container, false);
-        return view;
+        View v = inflater.inflate(R.layout.students, container, false);
+        this.list = v.findViewById(R.id.listViewRegisteredStudents);
+        reload();
+        this.listAdapter = new StudentListAdapter(v.getContext(), data);
+        this.refresh = v.findViewById(R.id.refresh);
+        this.list.setAdapter(listAdapter);
+        this.refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reload();
+                listAdapter.notifyDataSetChanged();
+            }
+        });
+        this.list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                View popUpView = inflater.inflate(R.layout.popup, null, false);
+                TextView name, surname, stuId, gender, faculty, department, advisor;
+                name = popUpView.findViewById(R.id.name);
+                surname = popUpView.findViewById(R.id.surname);
+                stuId = popUpView.findViewById(R.id.id);
+                gender = popUpView.findViewById(R.id.gender);
+                faculty = popUpView.findViewById(R.id.faculty);
+                department = popUpView.findViewById(R.id.department);
+                advisor = popUpView.findViewById(R.id.advisor);
+
+                HashMap current = data.get(position);
+
+                name.setText( (String) current.get("name"));
+                surname.setText( (String) current.get("surname"));
+                stuId.setText( (String) current.get("id"));
+                gender.setText( (String) current.get("gender"));
+                faculty.setText( (String) current.get("faculty"));
+                department.setText( (String) current.get("department"));
+                advisor.setText( (String) current.get("advisor"));
+
+                PopupWindow pw = new PopupWindow(popUpView,1000,700, true);
+
+                pw.showAtLocation(v, Gravity.CENTER, 0, 0);
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    void reload() {
+        Cursor cursor = db.getDatabase().query("student",null,null,null,null, null, null);
+        if (!this.data.isEmpty()) {
+            this.data.clear();
+        }
+        while (cursor.moveToNext()) {
+            HashMap map = new HashMap();
+            map.put("id", Long.toString(cursor.getLong(cursor.getColumnIndexOrThrow("id"))));
+            map.put("name", cursor.getString(cursor.getColumnIndexOrThrow("name")));
+            map.put("surname", cursor.getString(cursor.getColumnIndexOrThrow("surname")));
+            map.put("gender", cursor.getString(cursor.getColumnIndexOrThrow("gender")));
+            map.put("faculty", cursor.getString(cursor.getColumnIndexOrThrow("faculty")));
+            map.put("department", cursor.getString(cursor.getColumnIndexOrThrow("department")));
+            map.put("advisor", cursor.getString(cursor.getColumnIndexOrThrow("advisor")));
+            this.data.add(map);
+        }
     }
 }
---
+++ app/src/main/java/com/example/unidb/StudentListAdapter.java
@@ -0,0 +1,68 @@
+package com.example.unidb;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+class StudentListAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<HashMap> data;
+    private static LayoutInflater inflater = null;
+    int[] bgColors = new int[]{0xFFe5c3c6, 0xFFe1e9b7,0xFFbcd2d0,0xFF9bedff,0xFFfffd8d};
+
+    public StudentListAdapter(Context context, ArrayList<HashMap> data) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.data = data;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    public int getColor() {
+        int i = new Random().nextInt(bgColors.length);
+        return bgColors[i];
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View view = convertView;
+        if (view == null)
+            view = inflater.inflate(R.layout.list_item, null);
+        TextView id = view.findViewById(R.id.id);
+        TextView name = view.findViewById(R.id.name);
+        TextView surname = view.findViewById(R.id.surname);
+        LinearLayout wrapper = view.findViewById(R.id.wrapper);
+        wrapper.setBackgroundColor(getColor());
+        id.setText((String) data.get(position).get("id"));
+        name.setText((String) data.get(position).get("name"));
+        surname.setText((String) data.get(position).get("surname"));
+        return view;
+    }
+}
